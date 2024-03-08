40ad246f814faa13aca5262be04ec296064aa2bf
elianmedina <dev.game16@talentum.edu.co>
2024-03-08 14:48:37 -0500

    Navegación Personalizacion
    4.2.1 Navegación con Mouse en Personalización - Completada

--- Assets/Scripts/Personalization/PersonalizationEventManager.cs
+++ Assets/Scripts/Personalization/PersonalizationEventManager.cs
@@ -1,18 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
+using ActiveRagdoll;
 using UnityEngine;
 
 public class PersonalizationEventManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public GameObject player;
+    public GameObject[] opcionesPersonalizacion;
+
+    [SerializeField] float DesactiveAnimationModuleConstantTime = 0.41f;
+
+    private void Start()
     {
-        
+        Invoke("DesactiveAnimationModule", DesactiveAnimationModuleConstantTime);
+    }
+    public void OnSelectedCustomOption(int index)
+    {
+        for (int i = 0; i < opcionesPersonalizacion.Length; i++)
+        {
+            if(index == i)
+                opcionesPersonalizacion[i].SetActive(true);
+            else
+                opcionesPersonalizacion[i].SetActive(false);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    void DesactiveAnimationModule()
     {
-        
+        player.GetComponent<AnimationModule>().enabled = false;
     }
 }
