af75f9f00828dbf7a04e05e2bdbb88b2f7a9c6fb
Betsy <89090441+BetsyAlejandra@users.noreply.github.com>
2024-03-09 23:23:55 -0500

    aquitodofunciona

--- Assets/Scripts/CambioDeScena.cs
+++ Assets/Scripts/CambioDeScena.cs
@@ -1,60 +1,31 @@
-using System;
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class EventManager : MonoBehaviour
-{
-    public static event Action<string> OnSceneChangeRequested;
-
-    public static void RequestSceneChange(string sceneName)
-    {
-        OnSceneChangeRequested?.Invoke(sceneName);
-    }
-
-    public static event Action OnQuitRequested;
-
-    public static void RequestQuit()
-    {
-        OnQuitRequested?.Invoke();
-    }
-}
-
 public class CambioDeScena : MonoBehaviour
 {
-    private void Start()
-    {
-        EventManager.OnSceneChangeRequested += CambiarEscena;
-        EventManager.OnQuitRequested += Salir;
-    }
-
-    private void OnDestroy()
-    {
-        EventManager.OnSceneChangeRequested -= CambiarEscena;
-        EventManager.OnQuitRequested -= Salir;
-    }
-
     public void Bienvenida()
     {
-        EventManager.RequestSceneChange("Bienvenida");
+        SceneManager.LoadScene("Bienvenida");
     }
 
     public void IrNivel2()
     {
-        EventManager.RequestSceneChange("Nivel#2");
+        SceneManager.LoadScene("Nivel#2");
     }
 
     public void IrNivel3()
     {
-        EventManager.RequestSceneChange("Nivel#3");
+        SceneManager.LoadScene("Nivel#3");
     }
 
     public void Salir()
     {
-        EventManager.RequestQuit();
-    }
-
-    private void CambiarEscena(string sceneName)
-    {
-        SceneManager.LoadScene(sceneName);
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+            Application.Quit();
+#endif
     }
 }
