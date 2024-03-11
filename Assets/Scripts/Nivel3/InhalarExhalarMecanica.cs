6ca0c9c10b49427709761a28e69ba9f9e072c979
elianmedina <dev.game16@talentum.edu.co>
2024-03-11 15:00:00 -0500

    Inhalar/Aguantar/Exhalar mecánica terminada
    Cuenta con 2 sliders, uno que indica cuanto le falta para acabar de inhalar/aguantar/exhalar y otro que aparece después de inhalar y le toca aguantar o exhalar que le da 2 segundos para poder interactuar (si no lo hace se reinician los valores y le toca volver a inhalar)

--- Assets/Scripts/Nivel3/InhalarExhalarMecanica.cs
+++ Assets/Scripts/Nivel3/InhalarExhalarMecanica.cs
@@ -2,95 +2,165 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class InhalarExhalarMecanica : MonoBehaviour
 {
     public GameObject presionarTextGO;
+    public Slider sliderTiempo;
+    public Slider sliderTiempoParaPerder;
 
     [SerializeField]float timePressedInhalar, timePressedAguantar, timePressedExhalar;
-
     [SerializeField]float timerParaPerderAguantar = 2f, timerParaPerderExhalar = 2f;
 
     bool debeAguantarAire, debeExhalarAire;
+
+    [SerializeField] bool retoCompletado;
     private void Update()
     {
-        if (!debeAguantarAire)
+        if (!retoCompletado)
         {
-            if (!debeExhalarAire)
+            if (!debeAguantarAire)
             {
-                presionarTextGO.GetComponent<TMP_Text>().text = "Mantén presionado F para inhalar";
-                if (Input.GetKey(KeyCode.F))
+                if (!debeExhalarAire)
                 {
+                    sliderTiempo.value = timePressedInhalar;
+                    sliderTiempo.minValue = 0f;
+                    sliderTiempo.maxValue = 6f;
 
-                    presionarTextGO.GetComponent<Animator>().SetBool("presionando", true);
-                    timePressedInhalar += Time.deltaTime;
-                    if (timePressedInhalar > 6)
+                    sliderTiempoParaPerder.gameObject.SetActive(false);
+
+                    presionarTextGO.GetComponent<TMP_Text>().text = "Mantén presionado F para inhalar";
+                    if (Input.GetKey(KeyCode.F))
                     {
-                        timePressedInhalar = 6f;
-                        debeAguantarAire = true;
+
+                        presionarTextGO.GetComponent<Animator>().SetBool("presionando", true);
+                        timePressedInhalar += Time.deltaTime;
+                        if (timePressedInhalar > 6)
+                        {
+                            timePressedInhalar = 6f;
+                            debeAguantarAire = true;
+                        }
+                    }
+                    else
+                    {
+                        presionarTextGO.GetComponent<Animator>().SetBool("presionando", false);
+                        timePressedInhalar -= Time.deltaTime;
+                        if (timePressedInhalar < 0)
+                        {
+                            timePressedInhalar = 0f;
+                        }
                     }
                 }
                 else
                 {
-                    presionarTextGO.GetComponent<Animator>().SetBool("presionando", false);
-                    timePressedInhalar -= Time.deltaTime;
-                    if (timePressedInhalar < 0)
+                    sliderTiempo.value = timePressedExhalar;
+                    sliderTiempo.minValue = 0f;
+                    sliderTiempo.maxValue = 6f;
+
+                    sliderTiempoParaPerder.gameObject.SetActive(true);
+                    sliderTiempoParaPerder.value = timerParaPerderExhalar;
+                    sliderTiempoParaPerder.minValue = 0f;
+                    sliderTiempoParaPerder.maxValue = 2f;
+
+                    presionarTextGO.GetComponent<TMP_Text>().text = "Mantén presionado F para exhalar";
+                    if (Input.GetKey(KeyCode.F))
                     {
-                        timePressedInhalar = 0f;
+
+                        presionarTextGO.GetComponent<Animator>().SetBool("presionando", true);
+                        timePressedExhalar += Time.deltaTime;
+                        if (timePressedExhalar > 6)
+                        {
+                            timePressedExhalar = 6f;
+                            retoCompletado = true;
+                        }
+                    }
+                    else
+                    {
+                        presionarTextGO.GetComponent<Animator>().SetBool("presionando", false);
+                        timePressedExhalar -= Time.deltaTime;
+                        if (timePressedExhalar < 0)
+                        {
+                            timePressedExhalar = 0f;
+                        }
+                        if (timePressedExhalar == 0f)
+                        {
+                            timerParaPerderExhalar -= Time.deltaTime;
+
+                            if (timerParaPerderExhalar <= 0)
+                            {
+                                debeAguantarAire = false;
+                                debeExhalarAire = false;
+
+                                timerParaPerderExhalar = 2f; //Se reinicia el Timer 
+                                timerParaPerderAguantar = 2f; //Se reinicia el Timer
+
+                                timePressedInhalar = 0f;
+                                timePressedAguantar = 0f;
+                            }
+                        }
                     }
                 }
             }
             else
             {
-                presionarTextGO.GetComponent<TMP_Text>().text = "Mantén presionado F para exhalar";
-                if (Input.GetKey(KeyCode.F))
-                {
+                sliderTiempo.value = timePressedAguantar;
+                sliderTiempo.minValue = 0f;
+                sliderTiempo.maxValue = 3f;
 
+                sliderTiempoParaPerder.gameObject.SetActive(true);
+                sliderTiempoParaPerder.value = timerParaPerderAguantar;
+                sliderTiempoParaPerder.minValue = 0f;
+                sliderTiempoParaPerder.maxValue = 2f;
+
+                presionarTextGO.GetComponent<TMP_Text>().text = "Mantén presionado G para aguantare el aire";
+                if (Input.GetKey(KeyCode.G))
+                {
                     presionarTextGO.GetComponent<Animator>().SetBool("presionando", true);
-                    timePressedExhalar += Time.deltaTime;
-                    if (timePressedExhalar > 6)
+                    timePressedAguantar += Time.deltaTime;
+                    if (timePressedAguantar > 3)
                     {
-                        timePressedExhalar = 6f;
-                        Debug.Log("Completado");
+                        timePressedAguantar = 3f;
+                        debeAguantarAire = false;
+                        debeExhalarAire = true;
                     }
                 }
                 else
                 {
                     presionarTextGO.GetComponent<Animator>().SetBool("presionando", false);
-                    timePressedExhalar -= Time.deltaTime;
-                    if (timePressedExhalar < 0)
+                    timePressedAguantar -= Time.deltaTime;
+                    if (timePressedAguantar < 0)
                     {
-                        timePressedExhalar = 0f;
+                        timePressedAguantar = 0f;
                     }
-                    IniciarContadorSegundos(timerParaPerderExhalar, timePressedAguantar);
+                    if (timePressedAguantar == 0f)
+                    {
+                        timerParaPerderAguantar -= Time.deltaTime;
+
+                        if (timerParaPerderAguantar <= 0)
+                        {
+                            debeAguantarAire = false;
+                            debeExhalarAire = false;
+
+                            timerParaPerderExhalar = 2f; //Se reinicia el Timer 
+                            timerParaPerderAguantar = 2f; //Se reinicia el Timer
+
+                            timePressedInhalar = 0f;
+                            timePressedAguantar = 0f;
+                        }
+                    }
                 }
             }
         }
         else
         {
-            IniciarContadorSegundos(timerParaPerderAguantar, timePressedAguantar);
-            presionarTextGO.GetComponent<TMP_Text>().text = "Mantén presionado G para aguantare el aire";
-            if (Input.GetKey(KeyCode.G))
+            //RETO COMPLETADO
+           if(presionarTextGO != null)
             {
-                presionarTextGO.GetComponent<Animator>().SetBool("presionando", true);
-                timePressedAguantar += Time.deltaTime;
-                if (timePressedAguantar > 3)
-                {
-                    timePressedAguantar = 3f;
-                    debeAguantarAire = false;
-                    debeExhalarAire = true;
-                }
-            }
-            else
-            {
-                presionarTextGO.GetComponent<Animator>().SetBool("presionando", false);
-                timePressedAguantar -= Time.deltaTime;
-                if (timePressedAguantar < 0)
-                {
-                    timePressedAguantar = 0f;
-                }
-            }
+                presionarTextGO.SetActive(false);
+            }  
         }
+       
     }
 
     void IniciarContadorSegundos(float timer, float suTimer)
